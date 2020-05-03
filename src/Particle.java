5a4e60da4ccfd0506016f4023a6fbed74cccea22
Lewis Bonathan Metcalf <lewis.metcalf@thoughtworks.com>
2020-05-03 15:49:01 +0200

    changed particles to use doubles instead of floats, random velocity in both directions

--- src/Particle.java
+++ src/Particle.java
@@ -3,27 +3,29 @@ import processing.core.PApplet;
 import java.util.Random;
 
 public class Particle {
-    private float vx;
-    private float vy;
-    private float px;
-    private float py;
-    private final float radius;
+    private double vx;
+    private double vy;
+    private double px;
+    private double py;
+    private final double radius;
 
     public Particle(Random r) {
-        this.vx = r.nextFloat();
-        this.vy = r.nextFloat();
-        this.px = r.nextFloat() * 400;
-        this.py = r.nextFloat() * 400;
+        this.vx = r.nextDouble() - .5;
+        this.vy = r.nextDouble() - .5;
+        this.px = r.nextDouble() * 400;
+        this.py = r.nextDouble() * 400;
         this.radius = 10;
     }
 
-    public void move(float dt) {
+    public void move(double dt) {
+
+
         px = px + (dt * vx);
         py = py + (dt * vy);
     }
 
     public void draw(PApplet sketch) {
-        sketch.ellipse(px, py, radius, radius);
+        sketch.ellipse((float) px, ((float) py), ((float) radius), ((float) radius));
     }
 
 }
