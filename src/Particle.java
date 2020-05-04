7c50227948f860aba8f7e190b996d76fef25196e
Lewis Bonathan Metcalf <lewis.metcalf@thoughtworks.com>
2020-05-04 13:04:53 +0200

    approximation of naive impl, loss of kinetic energy and non exact collisions

--- src/Particle.java
+++ src/Particle.java
@@ -11,12 +11,12 @@ public class Particle {
     private final double mass;
 
     public Particle(Random r) {
-        this.radius = 15;
-        this.vx = r.nextDouble() - .5;
-        this.vy = r.nextDouble() - .5;
+        this.radius = 10;
+        this.vx = (r.nextDouble() - .5);
+        this.vy = (r.nextDouble() - .5);
         this.px = r.nextDouble() * (400 - radius*2) + radius;
         this.py = r.nextDouble() * (400 - radius*2) + radius;
-        this.mass = 1;
+        this.mass = 100;
     }
 
     public void move(double dt) {
@@ -28,26 +28,66 @@ public class Particle {
     }
 
     public void bounceOff(Particle other) {
-        double dx = other.px - this.px, dy = other.py - this.py;
-        double dvx = other.vx - this.vx, dvy = other.vy - this.vy;
-        double dvdr = dx*dvx + dy*dvy;
-        double dist = radius + other.radius;
-        double J = 2 * mass * other.mass * dvdr / ((mass + other.mass) * dist);
-        double Jx = J * dx / dist;
-        double Jy = J * dy / dist;
 
-        this.vx += Jx / this.mass;
-        this.vy += Jy / this.mass;
-        other.vx += Jx / other.mass;
-        other.vy += Jy / other.mass;
+        //roll back clock here...
+        double t = timeToHit(other);
+        move(t);
+
+        double dx  = other.px - this.px;
+        double dy  = other.py - this.py;
+        double dvx = other.vx - this.vx;
+        double dvy = other.vy - this.vy;
+        double dvdr = dx*dvx + dy*dvy;             // dv dot dr
+        double dist = this.radius + other.radius;   // distance between particle centers at collison
+
+        // magnitude of normal force
+        double magnitude = 2 * this.mass * other.mass * dvdr / ((this.mass + other.mass) * dist);
+
+        // normal force, and in x and y directions
+        double fx = magnitude * dx / dist;
+        double fy = magnitude * dy / dist;
+
+        // update velocities according to normal force
+        this.vx += fx / this.mass;
+        this.vy += fy / this.mass;
+        other.vx -= fx / other.mass;
+        other.vy -= fy / other.mass;
+
+        // roll forward again
+        move(-t);
+    }
+
+    public double timeToHit(Particle other) {
+        if (this == other) return Double.POSITIVE_INFINITY;
+        double dx  = other.px - this.px;
+        double dy  = other.py - this.py;
+        double dvx = other.vx - this.vx;
+        double dvy = other.vy - this.vy;
+        double dvdr = dx*dvx + dy*dvy;
+        if (dvdr > 0) return Double.POSITIVE_INFINITY;
+        double dvdv = dvx*dvx + dvy*dvy;
+        if (dvdv == 0) return Double.POSITIVE_INFINITY;
+        double drdr = dx*dx + dy*dy;
+        double sigma = this.radius + other.radius;
+        double d = (dvdr*dvdr) - dvdv * (drdr - sigma*sigma);
+//        if (d < 0) return Double.POSITIVE_INFINITY;
+        return -(dvdr + Math.sqrt(d)) / dvdv;
     }
 
     public void draw(PApplet sketch) {
         sketch.ellipse((float) px, ((float) py), ((float) radius), ((float) radius));
     }
 
-    public boolean overlaps(Particle other) {
-        return Math.abs(px - other.px) < radius
-            && Math.abs(py - other.py) < radius;
+    public boolean overlap(Particle other) {
+        if (this == other) return false;
+        double dx  = other.px - this.px;
+        double dy  = other.py - this.py;
+        double drdr = dx*dx + dy*dy;
+        double sigma = this.radius + other.radius;
+        return (drdr < sigma*sigma);
+    }
+
+    public double kineticEnergy() {
+        return 0.5 * mass * (vx*vx + vy*vy);
     }
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -7,24 +7,43 @@ public class Simulation {
     private final Particle[] particles;
 
     public Simulation() {
-        particles = new Particle[40];
+        particles = new Particle[100];
         Random randomGenerator = new Random();
+        //remove overlapping particles in setup
         for (int i = 0; i < particles.length; i++) {
             particles[i] = new Particle(randomGenerator);
+            while (overlapsOtherParticle(particles[i], i)) {
+                particles[i] = new Particle(randomGenerator);
+            }
         }
     }
 
+    private boolean overlapsOtherParticle(Particle particle, int end) {
+        for (int i = 0; i < end; i++) {
+            if (particle.overlap(particles[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void draw(PApplet sketch) {
+        double kineticSum = 0;
         for (Particle particle : particles) {
+
             //naive quadratic collision detection
             for (Particle other : particles) {
-                if (particle.overlaps(other)) {
+                if (particle.overlap(other)) {
                     particle.bounceOff(other);
                 }
             }
 
-            particle.move(2);
+            particle.move(0.5);
             particle.draw(sketch);
+
+            kineticSum += particle.kineticEnergy();
         }
+
+        System.out.println("kinetic sum: " + kineticSum);
     }
 }
